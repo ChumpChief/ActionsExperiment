ee7a1c664a25c5a9445b3c58adc7e2dbb6869edd
Matt Rakow <ChumpChief@users.noreply.github.com>
2024-03-18 19:28:51 -0700

    Try a version action (#9)

--- .github/workflows/invokableScript/invokableScript.js
+++ .github/workflows/invokableScript/invokableScript.js
@@ -1,4 +1,45 @@
-module.exports = async ({github, context, core}) => {
+module.exports = async ({github, context, core, exec}) => {
+    if (!context.ref.includes("refs/heads/")) {
+        throw new Error("Expected a branch!");
+    }
+    const baseBranchName = context.ref.replace("refs/heads/", "");
+    if (baseBranchName.includes("-bump")) {
+        throw new Error("Can't bump a bump branch!");
+    }
+    const bumpBranchName = `${ baseBranchName }-bump`;
+
+    const branchExists = (await exec.exec("git",  ["show-ref", "--quiet", `refs/heads/${ bumpBranchName }`], { ignoreReturnCode: true })) === 0;
+    if (!branchExists) {
+        // Create the branch and check it out
+        await exec.exec(`git checkout -b ${ bumpBranchName }`);
+    } else {
+        // Just check it out?  Reset?  Delete it?
+    }
+
+    // Run version bumping command
+    await exec.exec(`touch ${ Date.now() }.txt`);
+
+    // Commmit changes
+    await exec.exec(`git add .`);
+    await exec.exec(`git config --global user.name "github-actions[bot]"`);
+    await exec.exec(`git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"`);
+    await exec.exec(`git commit -m "Version bump"`);
+
+    // Push the branch
+    await exec.exec(`git push origin ${ bumpBranchName }`);
+
+    // Create a PR
+    const { data: newPullRequest } = await github.rest.pulls.create({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        head: bumpBranchName,
+        base: baseBranchName,
+        title: "Version bump",
+    });
+
+    console.log(`Opened PR #${newPullRequest.number}`);
+
+    // Wait for the PR to be merged (maybe a separate step/script)
     console.log(`${new Date().toLocaleString()}: taking a break...`);
     return new Promise((resolve) => {
         setTimeout(() => {
